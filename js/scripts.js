dc424a146bc58e4dfc3c5071c55048e57308924f
Janek Brandt and Mark Fisher <mark@>
2017-02-27 15:17:53 -0800

    add two methods to the prototype of ToDoItem and add clearDone function that removes items with doneStatus of true from the todo array

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,43 @@
 var todo = [];
 ToDoItem = function(text, doneStatus){
   this.text = text;
+  // this.time = time;
   this.doneStatus = doneStatus;
+};
+
+ToDoItem.prototype.setToDone = function(){
+  this.doneStatus = true;
 }
+
+ToDoItem.prototype.isDone = function(){
+  return(this.doneStatus === true);
+}
+
+var clearDone = function(arrayOfToDoItems){
+  var incompleteTaskArray = arrayOfToDoItems.filter(function(toDoItem){
+    return toDoItem.doneStatus === false;
+  });
+  console.log(incompleteTaskArray);
+  return incompleteTaskArray;
+}
+
 
 // Front End
 $(function(){
-  $("#add-btn").submit(function(){
+  // fix this
+  // $("#datetime-section").datetimepicker();
+  $("#add-form").submit(function(){
     event.preventDefault();
-    console.log("got here");
-    var usrInput = $("#submit-text").val();
+    var submittedText = $("#submit-text").val();
+    // var submittedTime = $
+    // validate, then
     var newItem = new ToDoItem(submittedText, false);
-    console.log(newItem);
+    todo.push(newItem);
+  });
+  $("#clear-form").submit(function(){
+    event.preventDefault();
+    todo[0].setToDone();
+    todo = clearDone(todo);
+    console.log(todo);
   });
 });
