66db773bb2d08c91330444fa8de42a84f76be502
Janek Brandt and Mark Fisher <janek@>
2017-02-27 16:06:19 -0800

    add setCheckedToDone function, which necessitated some reworking of addToDisplay. Still not pleased with it because of nested for loop

--- js/scripts.js
+++ js/scripts.js
@@ -14,14 +14,44 @@ ToDoItem.prototype.isDone = function(){
   return(this.doneStatus === true);
 }
 
-var clearDone = function(arrayOfToDoItems){
+var getIncomplete = function(arrayOfToDoItems){
   var incompleteTaskArray = arrayOfToDoItems.filter(function(toDoItem){
     return toDoItem.doneStatus === false;
   });
-  console.log(incompleteTaskArray);
+  // console.log(incompleteTaskArray);
   return incompleteTaskArray;
 }
 
+var addToDisplay = function(toDoItem){
+  $("#list-div").append("<div class='checkbox'>" +
+                        "<label> <input type='checkbox' name='toDo' value='" +  toDoItem.text + "'>" +
+                        toDoItem.text + "</label>");
+}
+
+var setCheckedToDone = function(arrayOfToDoItems){
+  $("input:checkbox[name=toDo]:checked").each(function(){
+    var toDoTxt = $(this).val();
+    for (var i = 0; i<arrayOfToDoItems.length; i++){
+      if(toDoTxt === arrayOfToDoItems[i].text){
+        // console.log(toDoTxt + " is " + arrayOfToDoItems[i].text);
+        arrayOfToDoItems[i].setToDone();
+      } else{
+        // console.log(arrayOfToDoItems[i].text + " isn't " + toDoTxt);
+      }
+    }
+  });
+  // console.log(arrayOfToDoItems);
+  // console.log(testArray);
+}
+
+var resetDisplayToOnlyIncomplete = function(arrayOfToDoItems){
+  arrayOfToDoItems = getIncomplete(arrayOfToDoItems);
+  console.log(arrayOfToDoItems);
+  $("#list-div").empty();
+  arrayOfToDoItems.forEach(function(item){
+    addToDisplay(item);
+  });
+}
 
 // Front End
 $(function(){
@@ -34,11 +64,14 @@ $(function(){
     // validate, then
     var newItem = new ToDoItem(submittedText, false);
     todo.push(newItem);
+    addToDisplay(newItem);
   });
   $("#clear-form").submit(function(){
     event.preventDefault();
-    todo[0].setToDone();
-    todo = clearDone(todo);
-    console.log(todo);
+    setCheckedToDone(todo);
+    // todo[0].setToDone();
+    resetDisplayToOnlyIncomplete(todo);
+
+    // console.log(todo);
   });
 });
